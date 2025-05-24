fb2d944c417406f902fd67694f4bc143773f9646
Ryoga <ttc2350st0006@edu.tech.ac.jp>
2025-05-24 11:49:08 +0900

    Add MUI

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import logo from './logo.svg';
+import {
+  Box,
+  Button,
+  Container,
+  Grid,
+  Link,
+  TextField,
+  Typography,
+} from "@mui/material";
+
+
+function App() {
+  return (
+        <Container maxWidth="xs">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography component="h1" variant="h4">
+          ログイン
+        </Typography>
+
+        <Box component="form" noValidate sx={{ mt:1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="メールアドレス"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="パスワード"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt:3, mb:2 }}
+          >
+            ログイン
+          </Button>
+
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                パスワードを忘れた
+              </Link>
+            </Grid>
+
+            <Grid item>
+              <Link href="#" variant="body2">
+                新規登録
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </Container>
+  );
+};
+
+export default App;
